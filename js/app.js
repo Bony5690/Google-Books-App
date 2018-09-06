2b735d3e6f50bf21822bde60edc2cae2dda57345
Bony5690 <jerrybony@yahoo.com>
2018-09-06 12:52:30 -0500

    Google books api
    Finished Google book api

--- js/app.js
+++ js/app.js
@@ -1,14 +1,63 @@
+// $(document).ready(function(){
+//   $("#myform").submit(function(){
+//
+//   var search = $("#books").val();
+//
+//    if(search == ''){
+//      alert("Please enter something in the field first");
+//    }
+//
+//    else {
+//      var url = '';
+//      var img = '';
+//      var title = '';
+//      var author = '';
+//      var publishing = '';
+//
+//     $.get(" https://www.googleapis.com/books/v1/volumes?q= " + search, function(response){
+//       console.log(response);
+//     });
+//
+//
+//    }
+//
+//
+//   });
+//
+//   return false;
+// });
+//
+     var url = '';
+     var img = '';
+     var title = '';
+     var author = '';
+     var publishing = '';
+
+
 function bookSearch(){
   var search = document.getElementById('search').value
   document.getElementById('results').innerHTML = ""
+  // document.getElementById('image').innerHTML = ""
   console.log(search);
 $.ajax({
   url: "https://www.googleapis.com/books/v1/volumes?q=" + search,
   dataType: "json",
 
   success: function(data){
-    for(var i = 0; i < data.items.length; i++){
-      results.innerHTML += "<h2>" + data.items[i].volumeInfo.title + "</h2>"
+    for(var i = 0; i < data.items.length; i++)
+    {
+    // title =      results.innerHTML += "<h2>" + data.items[i].volumeInfo.title +   "</h2>"
+    title = $('<h5 class = "center-align">' + data.items[i].volumeInfo.title + '</h5>');
+    author = $('<h5 class = "center-align">' + data.items[i].volumeInfo.authors + '</h5>');
+    publishing = $('<h5 class = "center-align">' + data.items[i].volumeInfo.publisher + '</h5>');
+    img = $('<img  class = "aligning"><br><a href=' + data.items[i].volumeInfo.infoLink + '><button class=" aligning btn btn-danger  ">Read more</button></a>');
+    url = data.items[i].volumeInfo.imageLinks.thumbnail;
+    img.attr('src', url); // attach the image url
+
+    title.appendTo("#results");
+    author.appendTo("#results");
+    publishing.appendTo("#results");
+    img.appendTo("#results");
       console.log(data);
     }
   },
