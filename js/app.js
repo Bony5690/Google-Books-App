85c5d36356d5afd4fbdcdd0b7f4ebdac58d55521
Bony5690 <jerrybony@yahoo.com>
2018-09-08 14:05:02 -0500

    Added conditional for no books found
    Render the text No books found when data.items is falsy

--- js/app.js
+++ js/app.js
@@ -1,71 +1,59 @@
-// $(document).ready(function(){
-//   $("#myform").submit(function(){
-//
-//   var search = $("#books").val();
-//
-//    if(search == ''){
-//      alert("Please enter something in the field first");
-//    }
-//
-//    else {
-//      var url = '';
-//      var img = '';
-//      var title = '';
-//      var author = '';
-//      var publishing = '';
-//
-//     $.get(" https://www.googleapis.com/books/v1/volumes?q= " + search, function(response){
-//       console.log(response);
-//     });
-//
-//
-//    }
-//
-//
-//   });
-//
-//   return false;
-// });
-//
      var url = '';
      var img = '';
      var title = '';
      var author = '';
      var publishing = '';
-
+     var message = '';
+     var search;
 
 function bookSearch(){
-  var search = document.getElementById('search').value
+   search = document.getElementById('search').value
   document.getElementById('results').innerHTML = ""
   // document.getElementById('image').innerHTML = ""
   console.log(search);
+
 $.ajax({
   url: "https://www.googleapis.com/books/v1/volumes?q=" + search,
   dataType: "json",
 
   success: function(data){
-    for(var i = 0; i < data.items.length; i++)
-    {
-    // title =      results.innerHTML += "<h2>" + data.items[i].volumeInfo.title +   "</h2>"
-    title = $('<h5 class = "center-align">' + data.items[i].volumeInfo.title + '</h5>');
-    author = $('<h5 class = "center-align">' + data.items[i].volumeInfo.authors + '</h5>');
-    publishing = $('<h5 class = "center-align">' + data.items[i].volumeInfo.publisher + '</h5>');
-    img = $('<img  class = "aligning"><br><a href=' + data.items[i].volumeInfo.infoLink + '><button class=" aligning btn btn-danger  ">Read more</button></a>');
-    url = data.items[i].volumeInfo.imageLinks.thumbnail;
-    img.attr('src', url); // attach the image url
+    if( !data.items){
+      message = $('<h4  class = "center-align"> Sorry no books found </h4>');
+     message.appendTo("#results");
+     console.log(data);
 
-    title.appendTo("#results");
-    author.appendTo("#results");
-    publishing.appendTo("#results");
-    img.appendTo("#results");
-      console.log(data);
+    } else {
+      for(var i = 0; i < data.items.length; i++)
+      {
+
+          title = $('<h5 class = "center-align"> Title: ' + data.items[i].volumeInfo.title + '</h5>');
+          author = $('<h5 class = "center-align"> Author: ' + data.items[i].volumeInfo.authors + '</h5>');
+          publishing = $('<h5 class = "center-align"> Publisher: ' + data.items[i].volumeInfo.publisher + '</h5>');
+          img = $('<img  class = "aligning"><br><a href=' + data.items[i].volumeInfo.infoLink + '><button class=" aligning btn btn-danger  ">Read more</button></a>');
+          url = data.items[i].volumeInfo.imageLinks.thumbnail;
+          img.attr('src', url); // attach the image url
+
+
+          title.appendTo("#results");
+          author.appendTo("#results");
+          publishing.appendTo("#results");
+          img.appendTo("#results");
+          // console.log(data);
+
+
+      }
+
+
     }
+
   },
 
   type: 'GET'
 })
 
 }
+
+
 
 
 
